9f6593d81e4c76f4570d224e8410a82e7f94ea4e
Andrew Mctague <officialbeardbandit@gmail.com>
2018-11-28 16:50:37 -0800

    Text Adventure Game
    Early stages of creating a text adventure.

---
+++ Text101/Text101/Assets/Scripts/State.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "State")]
+public class State : ScriptableObject {
+
+    // 14 determines the min size of our field 
+    // 10 is the min lines before scroll
+   [TextArea(10,14)] [SerializeField] string storyText;
+   [SerializeField] State[] nextStates;
+
+    public string GetStateStory() {
+        return storyText;
+    }
+
+    public State[] GetNextStates() {
+
+        return nextStates;
+
+    }
+
+
+}
